4c6e87bf9ce3f80bf4763ebf5db9b4ce804f899e
Aleksey Zvyagin <a.zvg@ya.ru>
2022-05-01 18:14:38 +0300

    client на python

--- install_depends.sh
+++ install_depends.sh
@@ -8,5 +8,14 @@ if [ ! -d .conan ]; then
     patch -p0 < conan.patch
 fi
 mkdir -p "$BUILD"
-cd "$BUILD"
+pushd "$BUILD"
 conan install .. --build=missing
+popd
+
+if [ ! -d .venv ]; then
+    python3 -m venv .venv
+fi
+. ./.venv/bin/activate
+pip3 install -r requirements.txt
+deactivate
+rm -rf .cache
